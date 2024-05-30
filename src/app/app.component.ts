a64f31c33ef6da08e8e393df6c318d4600458832
Bhushan Udupa <bhushanudupa@gmail.com>
2024-05-30 16:46:23 +0530

    Added posts code on 30-05-2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
